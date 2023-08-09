0a736d88ccdcc27f0434b0151e7e2c1cd2e17145
veronicasaz <veronica.saz.ulibarrena@gmail.com>
2023-08-09 17:04:01 +0200

    Energy error fixed

--- environments.py
+++ environments.py
@@ -10,6 +10,9 @@ from amuse.ic.kingmodel import new_king_model
 from amuse.community.hermite.interface import Hermite
 from amuse.community.bhtree.interface import BHTree
 
+from plots import plot_state, plot_trajectory
+
+units_G = units.m**3 * units.kg**(-1) * units.s**(-2)
 """
 https://www.gymlibrary.dev/content/environment_creation/
 """
@@ -32,33 +35,34 @@ class Cluster_2D(gym.Env):
         elif self.settings['Integration']['masses'] == 'equal_sun':
             m_stars = np.ones(self.n_stars) # each particle is 1 MSun
         else:
-            m_stars = self.settings['Integration']['masses']
+            m_stars = np.ones(self.n_stars) * self.settings['Integration']['masses']
 
         r_cluster = self.settings['Integration']['r_cluster'] | units.au
         self.converter = nbody_system.nbody_to_si(m_stars.sum() | units.MSun, r_cluster)
         W0 = 3
         bodies = new_king_model(self.n_stars, W0, convert_nbody = self.converter)
         bodies.scale_to_standard(self.converter)
-        bodies.mass = m_stars | units.kg
+        bodies.move_to_center()
+        bodies.mass = m_stars | units.MSun
 
         self.G = constants.G
 
         # Plot initial state
         if self.settings['Integration']['plot'] == True:
-            self.plot_state(bodies)
+            plot_state(bodies)
         return bodies
-    
+
     def _get_info(self):
         """
         get energy error, angular momentum error at current state
         """
-        E_kin = self.gravity.get_kinetic_energy().value_in(units.m**2 * units.s**(-2)* units.kg)
-        E_pot = self.gravity.get_potential_energy(self.G).value_in(units.m**2 * units.s**(-2)* units.kg)
+        E_kin = self.particles.kinetic_energy().value_in(units.m**2 * units.s**(-2)* units.kg)
+        E_pot = self.particles.potential_energy().value_in(units.m**2 * units.s**(-2)* units.kg)
         L = self.calculate_angular_m(self.gravity.particles)
-        Delta_E = E_kin + E_pot - self.E_0
-        Delta_L = L - self.L_0
-        # self.current_state
-        # TODO: calculate energy error and angular momentum error (we need initial state)
+        # Delta_E = E_kin + E_pot - self.E_0
+        Delta_E = E_kin + E_pot
+        # Delta_L = L - self.L_0
+        Delta_L = L
         return Delta_E, Delta_L
     
     def _get_integrator(self, index):
@@ -90,16 +94,15 @@ class Cluster_2D(gym.Env):
         observation = self.gravity
         info = self._get_info()
         self.iteration = 0
-        self.t_cumul = 0 # cumulative time for integration
+        self.t_cumul = 0.0 # cumulative time for integration
 
         # Initialize variables to save simulation
         if self.settings['Integration']['savestate'] == True:
-            steps = (self.settings['Integration']['check_step']-1) * \
+            steps = (self.settings['Integration']['check_step']) * \
                     self.settings['Integration']['max_steps']
-            print("Steps", steps)
-            self.state = np.zeros((steps, self.n_stars, 7)) # mass, rx3, vx3, 
-            self.cons = np.zeros((steps, 4)) # E, Lx3
-            self._savestate(0, self.particles, self.E_0, self.L_0) # save initial state
+            self.state = np.zeros((steps, self.n_stars, 8)) # action, mass, rx3, vx3, 
+            self.cons = np.zeros((steps, 5)) # action, E, Lx3
+            self._savestate(0, 0, self.particles, self.E_0, self.L_0) # save initial state
 
         return observation, info
     
@@ -108,35 +111,33 @@ class Cluster_2D(gym.Env):
         return W[0]*Delta_E + W[1]*Delta_O + W[2]*T
     
     def step(self, action):
-        
-        # Time steps
+
         check_step = self.settings['Integration']['check_step'] # after how many time steps it is checked
         t_step = self.settings['Integration']['t_step']
-        times = (np.arange(0, check_step*t_step, t_step) + self.t_cumul) | units.yr 
 
         # Apply action
         current_int = self.settings['Integration']['integrators'][action]
         if current_int != self.previous_int: # if it's different, change integrator
             self.gravity.stop()
             self.gravity = self._get_integrator(action)(self.converter)
-            self.channel = self.gravity.particles.new_channel_to(self.particles)
             self.gravity.particles.add_particles(self.particles)
+            self.channel = self.gravity.particles.new_channel_to(self.particles)
             self.t_cumul = 0
-        else:
-            self.t_cumul += check_step*t_step
+        elif current_int == self.previous_int and self.iteration > 0: # Do not accumulate for the first iter
+            self.t_cumul += t_step*check_step
 
-        # print("=========================================================")
-        # print(self.gravity.particles)
-        # print(self.particles)
-        
-        # Integrate 
+        # Time steps
+        # times = (np.arange(0, (check_step+1)*t_step, t_step) + self.t_cumul) | units.yr 
+        times = (np.arange(0, (check_step+1), 1)*t_step + self.t_cumul) | units.yr 
+
+        # Integrate
         t0 = time.time()
         for i, t in enumerate(times[1:]):
             self.gravity.evolve_model(t)
             self.channel.copy()
             if self.settings['Integration']['savestate'] == True:
                 info = self._get_info()
-                self._savestate(self.iteration*len(times[1:]) + i, self.particles, info[0], info[1]) # save initial state
+                self._savestate(action, self.iteration*len(times[1:]) + i, self.particles, info[0], info[1]) # save initial state
         T = time.time() - t0 # computation time 
         self.previous_int = current_int
         
@@ -152,22 +153,23 @@ class Cluster_2D(gym.Env):
             self._display_info(info)
 
         if self.settings['Integration']['plot'] == True:
-            self.plot_state(self.particles)
+            plot_state(self.particles)
 
         # finish experiment if max number of iterations is reached
         if (self.iteration == self.settings['Integration']['max_steps']):
             terminated = True
             self.gravity.stop()
-            self.plot_trajectory()
+            plot_trajectory(self.settings)
         else:
             terminated = False
 
         return self.particles, reward, terminated, False, info
     
     def close(self): #TODO: needed?
-        if self.window is not None:
-            pygame.display.quit()
-            pygame.quit()
+        # if self.window is not None:
+        #     pygame.display.quit()
+        #     pygame.quit()
+        plot_trajectory(self.settings)
 
     def calculate_angular_m(self, particles):
         """
@@ -179,8 +181,19 @@ class Cluster_2D(gym.Env):
             v = particles[i].velocity.value_in(units.m/units.s)
             m = particles[i].mass.value_in(units.kg)
             L += np.cross(r, m*v)
-
         return L
+    
+    def calculate_energy(self): # not needed, verified with amuse
+        ke = 0
+        pe = 0
+        for i in range(0, self.n_stars):
+            body1 = self.particles[i]
+            ke += 0.5 * body1.mass.value_in(units.kg) * np.linalg.norm(body1.velocity.value_in(units.m/units.s))**2
+            for j in range(i+1, self.n_stars):
+                body2 = self.particles[j]
+                pe -= self.G.value_in(units_G) * body1.mass.value_in(units.kg) * body2.mass.value_in(units.kg) / \
+                    np.linalg.norm(body2.position.value_in(units.m) - body1.position.value_in(units.m))
+        return ke, pe
 
     def _display_info(self, info):
         print("Iteration: %i/%i, E_E = %0.3E, E_L = %0.3E"%(self.iteration, \
@@ -188,60 +201,32 @@ class Cluster_2D(gym.Env):
                                  info[0],\
                                  np.linalg.norm(info[1])))
         
-    def _savestate(self, step, bodies, E, L):
-        self.state[step, :, 0] = bodies.mass.value_in(units.kg)
-        self.state[step, :, 1:4] = bodies.position.value_in(units.m)
-        self.state[step, :, 4:] = bodies.velocity.value_in(units.m/units.s)
-        self.cons[step, 0] = E
-        self.cons[step, 1:] = L
+    def _savestate(self, action, step, bodies, E, L):
+        self.state[step, :, 0] = action
+        self.state[step, :, 1] = bodies.mass.value_in(units.kg)
+        self.state[step, :, 2:5] = bodies.position.value_in(units.m)
+        self.state[step, :, 5:] = bodies.velocity.value_in(units.m/units.s)
+        self.cons[step, 0] = action
+        self.cons[step, 1] = E
+        self.cons[step, 2:] = L
 
         np.save(settings['Integration']['savefile'] +'_state', self.state)
         np.save(settings['Integration']['savefile'] +'_cons', self.cons)
 
 
-    def plot_state(self, bodies):
-        v = (bodies.vx**2 + bodies.vy**2 + bodies.vz**2).sqrt()
-        plt.scatter(bodies.x.value_in(units.au),\
-                    bodies.y.value_in(units.au), \
-                    c=v.value_in(units.kms), alpha=0.5)
-        plt.colorbar()
-        plt.show()
-
-    # def plot_error(self):
-        # plt.plot(bodies.x.value_in(units.au),\
-        #             bodies.y.value_in(units.au), \
-        #             c=v.value_in(units.kms), alpha=0.5)
-        # plt.colorbar()
-        # plt.show()
-
-    def plot_trajectory(self):
-        state = np.load(settings['Integration']['savefile'] +'_state.npy')
-        cons = np.load(settings['Integration']['savefile'] +'_cons.npy')
-
-        for i in range(self.n_stars):
-            plt.scatter(state[0, i, 1], state[0, i, 2], marker = 'o', s = 200)
-            plt.plot(state[:, i, 1], state[:, i, 2], marker = 'o')
-
-        plt.show()
-
-        t = np.arange(0, (self.settings['Integration']['check_step']-1) * \
-            self.settings['Integration']['t_step'] * self.settings['Integration']['max_steps'], \
-            self.settings['Integration']['t_step'])
-        plt.plot(t, cons[:, 0])
-        plt.plot(t, np.linalg.norm(cons[:, 1:], axis = 1))
-        plt.show()
-
+    
 settings = load_json("./settings.json")
 a = Cluster_2D(settings)
-a.reset()
 value = [0,1,1,0,0,1,0,0,0,0,1,1,1,0]
 # value = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 terminated = False
 i = 0
-while terminated == False:
-    print(value[i])
-    x, y, terminated, z, zz = a.step(value[i])
-    i += 1
+# a.reset()
+# while terminated == False:
+#     x, y, terminated, z, zz = a.step(value[i])
+#     i += 1
+
+a.close()
 
 # gym.envs.registration.register(
 #     id='gym_examples/Cluster2D-v0', # 
