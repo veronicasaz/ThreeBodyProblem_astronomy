abafb60536f8072183c526c6bff7f3458c904133
veronicasaz <veronica.saz.ulibarrena@gmail.com>
2023-08-22 17:03:13 +0200

    Some variables adapted

--- TestGym.py
+++ TestGym.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from IPython.display import clear_output
+import matplotlib
 import matplotlib.pyplot as plt
 
 import gym
@@ -25,6 +26,11 @@ env.reset() # Resets the environment and returns a random initial state
 
 # if GPU is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# set up matplotlib
+is_ipython = 'inline' in matplotlib.get_backend()
+if is_ipython:
+    from IPython import display
 
 ################################################################################
 # # Actions basic code
@@ -93,7 +99,6 @@ LR = 1e-4
 
 # Get number of actions from gym action space
 n_actions = env.action_space.n
-print(n_actions)
 # Get the number of state observations
 state, info = env.reset()
 n_observations = len(state)
@@ -123,32 +128,32 @@ def select_action(state):
         return torch.tensor([[env.action_space.sample()]], device=device, dtype=torch.long)
 
 
-episode_durations = []
+episode_rewards = []
 
 def plot_durations(show_result=False):
     plt.figure(1)
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    rewards_t = torch.tensor(episode_rewards, dtype=torch.float)
     if show_result:
         plt.title('Result')
     else:
         plt.clf()
         plt.title('Training...')
     plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
+    plt.ylabel('Reward')
+    plt.plot(rewards_t.numpy())
     # Take 100 episode averages and plot them too
-    if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-        means = torch.cat((torch.zeros(99), means))
-        plt.plot(means.numpy())
+    # if len(durations_t) >= 100:
+        # means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        # means = torch.cat((torch.zeros(99), means))
+        # plt.plot(means.numpy())
 
     plt.pause(0.001)  # pause a bit so that plots are updated
-    # if is_ipython:
-    #     if not show_result:
-    #         display.display(plt.gcf())
-    #         display.clear_output(wait=True)
-    #     else:
-    #         display.display(plt.gcf())
+    if is_ipython:
+        if not show_result:
+            display.display(plt.gcf())
+            display.clear_output(wait=True)
+        else:
+            display.display(plt.gcf())
 
 def optimize_model():
     if len(memory) < BATCH_SIZE:
@@ -195,6 +200,7 @@ def optimize_model():
     # In-place gradient clipping
     torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
     optimizer.step()
+
 if torch.cuda.is_available():
     num_episodes = 600
 else:
@@ -202,14 +208,15 @@ else:
 
 
 # Training loop
-for i_episode in range(num_episodes):
+for i_episode in range(settings['Training']['max_iter']):
+    print("Training episode: %i"%i_episode)
     # Initialize the environment and get it's state
     state, info = env.reset()
     state = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
-    for t in count():
+    for t in range(settings['Integration']['max_steps']):
         action = select_action(state)
-        observation, reward, terminated, info = env.step(action.item())
-        reward = torch.tensor([reward], device=device)
+        observation, reward_p, terminated, info = env.step(action.item())
+        reward = torch.tensor([reward_p], device=device)
         done = terminated 
 
         if terminated:
@@ -235,7 +242,7 @@ for i_episode in range(num_episodes):
         target_net.load_state_dict(target_net_state_dict)
 
         if done:
-            episode_durations.append(t + 1)
+            episode_rewards.append(reward_p)
             plot_durations()
             break
 
