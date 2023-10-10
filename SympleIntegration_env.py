d6e07100811a04a5d08c89dda750d4404290dbf8
veronicasaz <veronica.saz.ulibarrena@gmail.com>
2023-10-10 15:23:53 +0200

    Plots and orders fixed

--- SympleIntegration_env.py
+++ SympleIntegration_env.py
@@ -1,6 +1,7 @@
 import gym 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 import time
 from setuptools import setup
 import json
@@ -20,6 +21,10 @@ from plots import plot_state, plot_trajectory
 
 """
 https://www.gymlibrary.dev/content/environment_creation/
+
+Questions:
+why do the inner planets go slower?
+How does the time-scaling works? Not doing it with the correct units
 """
 def orbital_period(G, a, Mtot):
     return 2*np.pi*(a**3/(constants.G*Mtot)).sqrt()
@@ -36,9 +41,23 @@ class IntegrateEnv(gym.Env):
 
         self.suffix = suffix # added info for saving files
     
+        self._create_actions()
     # def _convert_units(self, converter, bodies, G):
     #     bodies.position = converter.to_nbody(bodies.position)
     #     print(bodies.position)
+
+    def _create_actions(self):
+        if self.settings['actions'] == 'mixed':
+            t_step = self.settings['Integration']['t_step']
+            order = self.settings['Integration']['order']
+            self.actions = list()
+            for i in range(len(order)):
+                for j in range(len(t_step)):
+                    self.actions.append((order[i], t_step[j]))
+        elif self.settings['actions'] == 'order':
+            self.actions = self.settings['Integration']['order']
+        elif self.settings['actions'] == 't_step':
+            self.actions = self.settings['Integration']['t_step']
 
     def _add_bodies(self, bodies):
         n_bodies = self.settings['Integration']['bodies']
@@ -62,6 +81,10 @@ class IntegrateEnv(gym.Env):
 
             bodies.add_particles(particle)
         
+        # remove extra solar system bodies
+        bodies_to_be_removed = ['MERCURY', 'JUPITER', 'SATURN', 'URANUS', 'NEPTUNE', 'PLUTO']
+        for b in range(len(bodies_to_be_removed)):
+            bodies.remove_particle(bodies[bodies.name==bodies_to_be_removed[b]])
         return bodies
 
 
@@ -101,27 +124,45 @@ class IntegrateEnv(gym.Env):
             plot_state(bodies)
         return bodies
 
+    def _get_info_initial(self):
+        """
+        get energy error, angular momentum error at current state
+        """
+        E_kin = self.gravity.particles.kinetic_energy().value_in(self.units_energy)
+        E_pot = self.gravity.particles.potential_energy(G = self.G).value_in(self.units_energy)
+        L = self.calculate_angular_m(self.gravity.particles)
+        return E_kin + E_pot, L
+    
     def _get_info(self):
         """
         get energy error, angular momentum error at current state
         """
-        E_kin = self.particles.kinetic_energy().value_in(self.units_energy)
-        E_pot = self.particles.potential_energy(G = self.G).value_in(self.units_energy)
+        E_kin = self.gravity.particles.kinetic_energy().value_in(self.units_energy)
+        E_pot = self.gravity.particles.potential_energy(G = self.G).value_in(self.units_energy)
         L = self.calculate_angular_m(self.gravity.particles)
         # Delta_E = E_kin + E_pot - self.E_0
-        Delta_E = E_kin + E_pot
+        Delta_E = (E_kin + E_pot - self.E_0) / self.E_0
         # Delta_L = L - self.L_0
-        Delta_L = L
+        Delta_L = (L - self.L_0) / self.L_0
         return Delta_E, Delta_L
-    
-    # def _get_integrator(self, index):
-    #     integrator = self.settings['Integration']['order'][index]
-    #     # gravity.parameters.timestep = (1./128)*Pin_0
-    #     return integrator
     
     def _strip_units(self, particles): #TODO
         state = np.zeros(())
         return state
+    
+    def _initialize_integrator(self, integrator, timestep, tstep_param):
+        if self.settings['Integration']['units'] == 'si':
+            g = symple(self.converter, redirection ='none')
+        else:
+            g = symple(redirection ='none')
+        g.initialize_code()
+        # g.parameters.set_defaults()
+
+        # g.parameters.epsilon_squared = (0.01| units.AU)**2
+        g.parameters.integrator = integrator
+        g.parameters.timestep_parameter = tstep_param
+        g.parameters.timestep = timestep | self.units_time
+        return g 
     
     def reset(self, options = None):
         seed = self.settings['Integration']['seed']
@@ -130,19 +171,25 @@ class IntegrateEnv(gym.Env):
         self.particles = self._initial_conditions(seed)
 
         # Initialize basic integrator and add particles
-        self.gravity = symple(self.converter)
-        self.gravity.parameters.integrator = self.settings['Integration']['order'][0]
-        self.gravity.parameters.timestep = 1e-4 | self.units_time
+        if self.settings['actions'] == 'mixed':
+            self.gravity = self._initialize_integrator(self.actions[0][0],\
+                                                self.actions[0][1], 0.01)
+        # elif self.settings['actions'] == 'order':
+        #     self.gravity = self._initialize_integrator(self.actions[0][0],\
+        #                                         self.actions[0][1], 0.01)
+        # else:
+
+        
 
         self.previous_int = self.settings['Integration']['order'][0]
 
         self.gravity.particles.add_particles(self.particles)
+        self.gravity.commit_particles()
+
         self.channel = self.gravity.particles.new_channel_to(self.particles)
 
         # Get initial energy and angular momentum. Initialize at 0 for relative error
-        self.E_0 = 0
-        self.L_0 = np.array([0,0,0])
-        self.E_0, self.L_0 = self._get_info()
+        self.E_0, self.L_0 = self._get_info_initial()
 
         # state = self.gravity.particles
         state = [0, 0, 0, 0, 0] # Computation time, Error_0, L_0x3
@@ -152,8 +199,7 @@ class IntegrateEnv(gym.Env):
 
         # Initialize variables to save simulation
         if self.settings['Integration']['savestate'] == True:
-            steps = (self.settings['Integration']['check_step']) * \
-                    self.settings['Integration']['max_steps']
+            steps = self.settings['Integration']['max_steps']
             self.state = np.zeros((steps, self.n_bodies, 8)) # action, mass, rx3, vx3, 
             self.cons = np.zeros((steps, 5)) # action, E, Lx3, 
             self.comp_time = np.zeros((self.settings['Integration']['max_steps'], 1)) # computation time
@@ -167,39 +213,41 @@ class IntegrateEnv(gym.Env):
     
     def step(self, action):
 
-        check_step = self.settings['Integration']['check_step'] # after how many time steps it is checked
-        t_step = self.settings['Integration']['t_step'][1] # TODO: modify to vary this as well
+        check_step = self.settings['Integration']['check_step'] # final time for step integration
 
         t0_step = time.time()
+        print("Action", self.actions[action])
 
         # Apply action
-        current_int = self.settings['Integration']['order'][action]
-        if current_int != self.previous_int: # if it's different, change integrator
-            # self.gravity.stop()
-            # self.gravity = self._get_integrator(action)()
-            # self.gravity.particles.add_particles(self.particles) #TODO: needed again?
-            self.gravity.parameters.integrator = current_int
-            # self.channel = self.gravity.particles.new_channel_to(self.particles)
-            self.t_cumul = 0
-        elif current_int == self.previous_int and self.iteration > 0: # Do not accumulate for the first iter
-            self.t_cumul += t_step*check_step
-
-        # Time steps
-        times = (np.arange(0, (check_step+1), 1)*t_step + self.t_cumul) | self.units_time
-
+        current_int = self.actions[action][0]
+        t_step = self.actions[action][1]
+        # if current_int != self.previous_int: # if it's different, change integrator
+            # TODO: can this be done without reinitializing?
+        self.gravity.parameters.integrator = current_int
+        self.gravity.parameters.timestep = t_step | self.units_time
+            # self.t_cumul = 0
+        # elif current_int == self.previous_int and self.iteration > 0: # Do not accumulate for the first iter
+        self.t_cumul += check_step
         # Integrate
-        for i, t in enumerate(times[1:]):
-            self.gravity.evolve_model(t)
-            self.channel.copy()
-            if self.settings['Integration']['savestate'] == True:
-                info = self._get_info()
-                self._savestate(action, self.iteration*len(times[1:]) + i, self.particles, info[0], info[1]) # save initial state
+        print("Iteration", self.iteration)
+        # self.gravity.evolve_model(check_step | self.units_time)
+        print(self.gravity.particles[3])
+        self.gravity.evolve_model(self.t_cumul | self.units_time)
+        self.channel.copy()
+        print(self.gravity.particles[3])
+
+        # for i, t in enumerate(times[1:]):
+            # self.gravity.evolve_model(t)
+            # self.channel.copy()
+        if self.settings['Integration']['savestate'] == True:
+            info = self._get_info()
+            self._savestate(action, self.iteration, self.gravity.particles, info[0], info[1]) # save initial state
         self.previous_int = current_int
         
         # Get information for the reward
         T = time.time() - t0_step
         info = self._get_info()
-        state = [T, info[0], info[1][0], info[1][1], info[1][2]]
+        state = [T, info[0], info[1][0], info[1][1], info[1][2]] # TODO: change to particles positions and velocities
         reward = self._calculate_reward(info, T, self.W) # Use computation time for this step, including changing integrator
 
         self.iteration += 1
@@ -213,7 +261,7 @@ class IntegrateEnv(gym.Env):
             self._display_info(info)
 
         if self.settings['Integration']['plot'] == True:
-            plot_state(self.particles)
+            plot_state(self.gravity.particles)
 
         # finish experiment if max number of iterations is reached
         if (self.iteration == self.settings['Integration']['max_steps']):
@@ -285,7 +333,7 @@ class IntegrateEnv(gym.Env):
         n_bodies = np.shape(state)[1]
 
         for i in range(n_bodies):
-            plt.plot(state[:, i, 2], state[:, i, 3], label = self.names[i])
+            plt.plot(state[:, i, 2], state[:, i, 3], marker= 'o', label = self.names[i])
         plt.legend()
         plt.show()
 
@@ -306,6 +354,9 @@ def run_many_symple_cases(values):
     cases = len(values)
     a = IntegrateEnv()
     for j in range(cases):
+        print("Case %i"%j)
+        print(values[j][0])
+        print(a.actions[values[j][0]])
         a.suffix = ('_case%i'%j)
         value = values[j]
         terminated = False
@@ -315,8 +366,11 @@ def run_many_symple_cases(values):
             x, y, terminated, zz = a.step(value[i%len(value)])
             i += 1
         a.close()
+        # a.plot_orbit()
 
-def evaluate_many_symple_cases(values):
+def evaluate_many_symple_cases(values, \
+                        plot_tstep = True, \
+                        plot_grid = True):
     cases = len(values)
     a = IntegrateEnv()
 
@@ -343,51 +397,97 @@ def evaluate_many_symple_cases(values):
     E_E = np.zeros((steps, cases))
     E_M = np.zeros((steps, cases))
     for i in range(cases):
-        E_E[:, i] = abs((cons[i][:, 1] - cons[i][0, 1])/ cons[i][0, 1]) # absolute relative energy error
-        E_M[:, i] = np.linalg.norm((cons[i][:, 2:] - cons[i][0, 2:])/ cons[i][0, 2:], axis = 1) # relative angular momentum error
+        # E_E[:, i] = abs((cons[i][:, 1] - cons[i][0, 1])/ cons[i][0, 1]) # absolute relative energy error
+        # E_M[:, i] = np.linalg.norm((cons[i][:, 2:] - cons[i][0, 2:])/ cons[i][0, 2:], axis = 1) # relative angular momentum error
+        E_E[:, i] = abs(cons[i][:, 1]) # absolute relative energy error
+        E_M[:, i] = np.linalg.norm((cons[i][:, 2:] - cons[i][0, 2:]), axis = 1) # relative angular momentum error
 
 
     # plot
     colors = ['red', 'green', 'blue', 'orange', 'grey', 'yellow', 'black']
-    labels = ['Order 1', 'Order 2', 'Order 3', 'Order 5', 'Order 10', 'Mixed order']
+    lines = ['-', '--', ':', '-.', '-', '--', ':', '-.' ]
+    # labels = ['Order 1', 'Order 2', 'Order 3', 'Order 5', 'Order 10', 'Mixed order']
+    n_order_cases = len(a.settings['Integration']['order'])
+    n_tstep_cases = len(a.settings['Integration']['t_step'])
 
-    fig, ax = plt.subplots(2, 1, layout = 'constrained')
-    x_axis = np.arange(0, steps, 1)
-    for i in range(cases):
-        ax[0].plot(x_axis, E_E[:, i], color = colors[i], label = labels[i])
-        ax[1].plot(x_axis, E_M[:, i], color = colors[i], label = labels[i])
+    if plot_tstep == True:
+        fig, ax = plt.subplots(2, 1, layout = 'constrained', figsize = (10, 10))
+        x_axis = np.arange(0, steps, 1)
 
-    ax[0].legend()
-    ax[0].set_yscale('log')
-    ax[0].set_ylabel('Energy error')
-    ax[1].set_ylabel('Angular momentum error')
-    ax[1].set_xlabel('Step')
-    plt.show()
+        for i in range(cases):
+            ax[0].plot(x_axis, E_E[:, i], color = colors[i//n_tstep_cases], linestyle = lines[i%n_tstep_cases], label = 'O%i, t_step = %1.1E'%(a.actions[values[i][0]][0], a.actions[values[i][0]][1]))
+            ax[1].plot(x_axis, E_M[:, i], color = colors[i//n_tstep_cases], linestyle = lines[i%n_tstep_cases], label = 'O%i, t_step = %1.1E'%(a.actions[values[i][0]][0], a.actions[values[i][0]][1]))
+
+        labelsize = 20
+        ax[0].legend(loc='upper right')
+        ax[0].set_ylabel('Energy error',  fontsize = labelsize)
+        ax[1].set_ylabel('Angular momentum error', fontsize = labelsize)
+        ax[1].set_xlabel('Step', fontsize = labelsize)
+        for pl in range(len(ax)):
+            ax[pl].set_yscale('log')
+            ax[pl].tick_params(axis='both', which='major', labelsize=labelsize)
+        plt.show()
     
+    if plot_grid == True:
+        fig, ax = plt.subplots(2, 1, layout = 'constrained', figsize = (10, 10))
+        print(a.actions)
+        print(np.array(a.actions)[:,0])
+        print(E_E[-1,:])
+
+        # for i in range(cases): #Excluding the case with the mixed actions
+        sc = ax[0].scatter(np.array(a.actions)[:,0], np.array(a.actions)[:,1], marker = 'o',\
+                           s = 500, c = E_E[-1,:-1], cmap = 'RdYlBu', \
+                            norm=matplotlib.colors.LogNorm())
+        plt.colorbar(sc, ax = ax[0])
+
+        sm = ax[1].scatter(np.array(a.actions)[:,0], np.array(a.actions)[:,1], marker = 'o',\
+                           s = 500, c = E_M[-1,:-1], cmap = 'RdYlBu', \
+                            norm=matplotlib.colors.LogNorm())
+        plt.colorbar(sm, ax = ax[1])
+
+        labelsize = 20
+        ax[0].legend(loc='upper right')
+        ax[0].set_ylabel('t_step',  fontsize = labelsize)
+        ax[0].set_title("Final Energy error", fontsize = labelsize)
+        ax[1].set_ylabel('t_step', fontsize = labelsize)
+        ax[1].set_xlabel('Order', fontsize = labelsize)
+        ax[0].set_title("Final Angular momentum error", fontsize = labelsize)
+        for pl in range(len(ax)):
+            ax[pl].set_yscale('log')
+            ax[pl].tick_params(axis='both', which='major', labelsize=labelsize)
+        plt.show()
+
+def test_1_case():
+    a = IntegrateEnv()
+    terminated = False
+    i = 0
+    a.reset()
+    value = values[0]
+    print(value)
+
+    while terminated == False:
+        x, y, terminated, zz = a.step(value[i%len(value)])
+        i += 1
+    a.close()
+    a.plot_orbit()
 
 
-# settings = load_json("./settings.json")
+# Run all possibilities
+a = IntegrateEnv()
+steps = 30 # large value just in case
+values = list()
+for i in range(len(a.actions)):
+    values.append([i]*steps)
 
-steps = 200 # large value just in case
-value1 = [0]*steps
-value2 = [1]*steps
-value3 = [2]*steps
-value4 = [3]*steps
-value5 = [4]*steps
-value6 = np.random.randint(0, 5, size = steps)
-values = [value1, value2, value3, value4, value5, value6]
+# values = values[0:4]
+values.append(np.random.randint(0, len(a.actions), size = steps))
 # run_many_symple_cases(values)
-evaluate_many_symple_cases(values)
+evaluate_many_symple_cases(values, \
+                           plot_tstep = False, \
+                           plot_grid = True)
+
+# test_1_case()
 
 
-# a = IntegrateEnv()
-# terminated = False
-# i = 0
-# a.reset()
-# while terminated == False:
-#     x, y, terminated, zz = a.step(value[i%len(value)])
-#     i += 1
-# a.close()
 
-# a.plot_orbit()
 
